8c9664a4c3888a6326a739bfa2ed1508bbd4b80e
Anton Dementyev <knoexus@yahoo.com>
2020-01-13 17:04:29 +0300

    profile

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../types'
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED, LOADING_USER } from '../types'
 import axios from 'axios'
 
 export const loginUser = (userData, history) => dispatch => {
@@ -50,12 +50,40 @@ export const logoutUser = () => dispatch => {
 }
 
 export const getUserData = () => dispatch => {
+    dispatch({
+        type: LOADING_USER
+    })
+
     axios.get('/user')
         .then(res => {
             dispatch({
                 type: SET_USER,
                 payload: res.data
             })
+        })
+        .catch(err => console.error(err))
+}
+
+export const uploadImage = formData => dispatch => {
+    dispatch({
+        type: LOADING_USER
+    })
+
+    axios.post('/user/image', formData)
+        .then(() => {
+            dispatch(getUserData())
+        })
+        .catch(err => console.error(err))
+}
+
+export const editUserDetails = userDetails => dispatch => {
+    dispatch({
+        type: LOADING_USER
+    })
+    
+    axios.post('/user', userDetails)
+        .then(() => {
+            dispatch(getUserData())
         })
         .catch(err => console.error(err))
 }

5405aa662b4dbbb098489e10eb11e1a25d5fc88a
Anton Dementyev <knoexus@yahoo.com>
2020-01-15 16:28:00 +0300

    dialog like redux fix

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -30,7 +30,10 @@ export default function(state = initialState, action){
             let index = state.screams.findIndex(scream => scream.screamId === action.payload.screamId)
             state.screams[index] = action.payload
             if(state.scream.screamId === action.payload.screamId){
-                state.scream = action.payload
+                state.scream = {
+                    ...state.scream,
+                    ...action.payload
+                }
             }
             return {
                 ...state
